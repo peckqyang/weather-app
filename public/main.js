b0ee148ebcd1ca732de3a4ba23a7c8d9c674ce6b
Peck Yang <peckqyang@users.noreply.github.com>
2021-04-17 18:37:23 -0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,13 +1,4 @@
-/*
 
-- Sign up for openweathermap.org and generate an API key.
-- User either $.ajax or $.get to pull weather current data .
-  for San Francisco (hint: http://api.openweathermap.org/data/2.5/weather?q=...).
-- Print the temperature in console.
-- Bonus 1: add a form prompting user for the city and state.
-- Bonus 2: convert answer from kelvin to fahrenheit.
-
-*/
 
 'use strict';
 
